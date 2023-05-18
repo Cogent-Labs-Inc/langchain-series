bb9b2a8a58d245d7e58433ae5471d891e0b05519
hamzaJaral <hamzajarral62@gmail.com>
2023-05-18 17:48:52 +0500

    code refactored

--- 001_ed_tech_quiz/app.py
+++ 001_ed_tech_quiz/app.py
@@ -1,74 +1,55 @@
 import json
-
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import SequentialChain
 import streamlit as st
-import PyPDF2
 import traceback
+import pandas as pd
 from langchain.callbacks import get_openai_callback
+from utils import parse_file, get_table_data, RESPONSE_JSON
 
 load_dotenv()
 
-
-def parse_file(file):
-    if file.name.endswith('.pdf'):
-        try:
-            pdf_reader = PyPDF2.PdfReader(file)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            return text
-        except PyPDF2.utils.PdfReadError:
-            raise Exception("Error reading the PDF file.")
-
-    elif file.name.endswith('.txt'):
-        return file.read().decode('utf-8')
-
-    else:
-        raise Exception("Unsupported file format. Only PDF and TXT files are supported.")
-
-
 # This is an LLMChain to create 10-20 multiple choice questions from a given piece of text.
-llm = OpenAI(model_name='gpt-3.5-turbo', temperature=0, max_tokens=-1)
-
-response_json = json.dumps({
-    "1": {"no": "1", "mcq": "multiple choice question",
-          "options": {"a": "choice here", "b": "choice here", "c": "choice here", "d": "choice here"},
-          "correct": "correct answer"},
-    "2": {"no": "2", "mcq": "multiple choice question",
-          "options": {"a": "choice here", "b": "choice here", "c": "choice here", "d": "choice here"},
-          "correct": "correct answer"},
-    "3": {"no": "3", "mcq": "multiple choice question",
-          "options": {"a": "choice here", "b": "choice here", "c": "choice here", "d": "choice here"},
-          "correct": "correct answer"}
-})
+llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=-1)
 
 template = """
 Text: {text}
-You are an expert MCQ maker. Given the above text, it is your job to create a quiz of {number} multiple choice questions for grade {grade} students in {tone} tone.
+You are an expert MCQ maker. Given the above text, it is your job to\
+create a quiz of {number} multiple choice questions for grade {grade} students in {tone} tone.
 Make sure that questions are not repeated and check all the questions to be conforming to the text as well.
-Make sure to format your response like the RESPONSE_JSON below and use it as a guide. Ensure to make the {number} MCQs.
+Make sure to format your response like the RESPONSE_JSON below and use it as a guide.\
+Ensure to make the {number} MCQs.
 ### RESPONSE_JSON
 {response_json}
 """
-quiz_generation_prompt = PromptTemplate(input_variables=["text", "number", "grade", "tone", "response_json"],
-                                        template=template)
-quiz_chain = LLMChain(llm=llm, prompt=quiz_generation_prompt, output_key="quiz", verbose=True)
+quiz_generation_prompt = PromptTemplate(
+    input_variables=["text", "number", "grade", "tone", "response_json"],
+    template=template,
+)
+quiz_chain = LLMChain(
+    llm=llm, prompt=quiz_generation_prompt, output_key="quiz", verbose=True
+)
 
 # This is an LLMChain to evaluate the multiple choice questions created by the above chain
-llm = OpenAI(model_name='gpt-3.5-turbo', temperature=0)
-template = """You are an expert english grammarian and writer. Given a multiple choice quiz for {grade} grade students. You need to evaluate complexity of the questions and give a complete analysis of the quiz if the students 
+llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+template = """You are an expert english grammarian and writer. Given a multiple choice quiz for {grade} grade students.\
+You need to evaluate complexity of the questions and give a complete analysis of the quiz if the students 
 will be able to understand the questions and answer them. Only use at max 50 words for complexity analysis.
-If quiz is not at par with the cognitive and analytical abilities of the students, update the quiz questions which need to be changed and change the tone such that it perfectly fits the students abilities. 
+If quiz is not at par with the cognitive and analytical abilities of the students,\
+update the quiz questions which need to be changed and change the tone such that it perfectly fits the students abilities. 
 Quiz MCQs:
 {quiz}
 Critique from an expert english writer of the above quiz:"""
 
-quiz_evaluation_prompt = PromptTemplate(input_variables=["grade", "quiz"], template=template)
-review_chain = LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True)
+quiz_evaluation_prompt = PromptTemplate(
+    input_variables=["grade", "quiz"], template=template
+)
+review_chain = LLMChain(
+    llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True
+)
 
 # This is the overall chain where we run these two chains in sequence.
 generate_evaluate_chain = SequentialChain(
@@ -76,9 +57,10 @@ generate_evaluate_chain = SequentialChain(
     input_variables=["text", "number", "grade", "tone", "response_json"],
     # Here we return multiple variables
     output_variables=["quiz", "review"],
-    verbose=True)
+    verbose=True,
+)
 
-st.title('🦜⛓️ Langchain-Series: 001-Quiz Generation for Educational Content')
+st.title("🦜⛓️ Langchain-Series: 001-Quiz Generation for Educational Content")
 
 # Create a form using st.form
 with st.form("user_inputs"):
@@ -94,45 +76,42 @@ with st.form("user_inputs"):
 
 # Check if the button is clicked and all fields have inputs
 if button and uploaded_file is not None and mcq_count and grade and tone:
-    try:
-        text = parse_file(uploaded_file)
-
-        # count tokens and cost of api call
-        with get_openai_callback() as cb:
-            response = generate_evaluate_chain({
-                "text": text,
-                "number": mcq_count,
-                "grade": grade,
-                "tone": tone,
-                "response_json": response_json
-            })
-    except Exception as e:
-        st.error(traceback.print_exc())
-    else:
-        print(f"Total Tokens: {cb.total_tokens}")
-        print(f"Prompt Tokens: {cb.prompt_tokens}")
-        print(f"Completion Tokens: {cb.completion_tokens}")
-        print(f"Total Cost (USD): ${cb.total_cost}")
-
-    if isinstance(response, dict):
+    with st.spinner("Loading..."):
         try:
-            # Extract quiz data from the response
-            quiz_data_str = response.get("quiz", None)
-            if quiz_data_str is not None:
-                # convert the quiz from a str to dict
-                quiz_dict = json.loads(quiz_data_str)
-                quiz_table_data = []
-                # Iterate over the quiz dictionary and extract the required information
-                for key, value in quiz_dict.items():
-                    mcq = value["mcq"]
-                    options = " | ".join(
-                        [f"{option}: {option_value}" for option, option_value in value["options"].items()])
-                    correct = value["correct"]
-                    quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
-                st.table(quiz_table_data)
-                # Display the review in a text box
-                st.text_area(label="Review", value=response['review'])
+            text = parse_file(uploaded_file)
+
+            # count tokens and cost of api call
+            with get_openai_callback() as cb:
+                response = generate_evaluate_chain(
+                    {
+                        "text": text,
+                        "number": mcq_count,
+                        "grade": grade,
+                        "tone": tone,
+                        "response_json": json.dumps(RESPONSE_JSON),
+                    }
+                )
         except Exception as e:
-            traceback.print_exc()
-    else:
-        st.write(response)
+            traceback.print_exception(type(e), e, e.__traceback__)
+            st.error("Error")
+        else:
+            print(f"Total Tokens: {cb.total_tokens}")
+            print(f"Prompt Tokens: {cb.prompt_tokens}")
+            print(f"Completion Tokens: {cb.completion_tokens}")
+            print(f"Total Cost (USD): ${cb.total_cost}")
+
+            if isinstance(response, dict):
+                # Extract quiz data from the response
+                quiz = response.get("quiz", None)
+                if quiz is not None:
+                    table_data = get_table_data(quiz)
+                    if table_data is not None:
+                        df = pd.DataFrame(table_data)
+                        df.index = df.index + 1
+                        st.table(df)
+                        # Display the review in a text box
+                        st.text_area(label="Review", value=response["review"])
+                    else:
+                        st.error("Error in table data")
+            else:
+                st.write(response)
